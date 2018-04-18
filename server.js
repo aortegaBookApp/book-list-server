2c6062014bc1ad7b1a367564b098c2fc61f68bc0
Ana Ortega <anaa0hh@icloud.com>
2018-04-18 15:49:30 -0700

    server almost done

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+'use strict';
+
+// require('dotenv').config();
+
+//APPLICATION DEPENDENCIES
 const express = require('express');
-const PORT = process.env.PORT;
+const cors = require('cors');
+const pg = require('pg');
 
+//APPLICATION SETUP
 const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
 
-app.get('/test', (req, res) => 
-  res.send('hello world'));
+//DATABASE SETUP
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err)); //This will catch an error state
 
-app.listen(PORT, () => console.log('Listening on PORT', PORT));
+
+//API ENDPOINTS
+app.get('/api/v1/books', (req, res) =>  res.send('Testing 1, 2, 3'));
+//This app.get will need more once operational
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
